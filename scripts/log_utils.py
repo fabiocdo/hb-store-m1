04904b2151327cbcba073e8d72612b6ed1290a4e
Fabio Carvalho de Oliveira <fabio.cdo94@gmail.com>
2026-01-27 20:18:06 -0300

    refactored

--- scripts/log_utils.py
+++ scripts/log_utils.py
@@ -4,8 +4,8 @@ LOGGER = logging.getLogger()
 COLORS = {
     "created": "\033[0;32m",
     "modified": "\033[0;33m",
-    "deleted": "\033[0;91m",
-    "error": "\033[1;95m",
+    "deleted": "\033[0;33m",
+    "error": "\033[1;35m",
     "info": "\033[0m",
     "default": "\033[0m",
 }
